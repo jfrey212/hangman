9889f4b1015d3070b47a30ca7b97093505f07d1b
jfrey212 <eojyerf@gmail.com>
2022-05-02 22:24:22 -0500

    Added print_hangman method; added methods and instance variables to Game class

--- game.rb
+++ game.rb
@@ -2,13 +2,29 @@
 
 # Game class stores state of the hangman game
 class Game
-  def initialize(name, word)
+  def initialize
     @name = name
     @word = word
-    @guess_count = 0
-    @wrong_letters = []
+    @count = 7
+    @guessed_letters = []
     @word_progress = Array.new(word.length, '_')
   end
 
-  attr_reader :name, :word
+  attr_accessor :count, :guessed_letters, :word_progress, :name, :word
+
+  def check_guess(guess)
+
+  end
+
+  def check_end_game
+    if count == 0 && @word_progress.join != @word
+      return 'lose'
+    elsif @word_progress.join == @word
+      return 'win'
+    end
+  end
+
+  def save_game;end
+
+  def load_game;end
 end
--- main.rb
+++ main.rb
@@ -9,16 +9,24 @@ def dictionary_load
   File.open('dictionary.txt', 'r', &:readlines).map(&:chomp!).select { |word| word.length >= 5 && word.length <= 12 }
 end
 
-def check_letter(letter, word_progress, word)
-
+def print_hangman(guess)
+  puts "|-----#{guess == 0 ? '|' : '-'}"
+  puts "|     #{'|' if guess == 0}"
+  puts "|     #{'O' if guess <= 6}"
+  puts "|   #{'  ' if guess > 2}#{'--' if guess <= 2}#{'|' if guess <= 5}#{'--' if guess <= 1}"
+  puts "|     #{'|' if guess <= 5}"
+  puts "|    #{'/' if guess <= 4} #{'\\' if guess <= 3}"
+  puts '|'
+  puts '|___________'
 end
 
-def print_hangman(guess_count)
-
-end
-
-def print_turn(word, guess_count, wrong_letters, word_progress)
-
+def print_status(guessed_letters, guess_left, word_progress)
+  word_progress.each { |letter| print "#{letter} "}
+  puts
+  puts "Guesses Remaining: #{guesses_left}"
+  print 'Guessed Letters: '
+  guessed_letters.each { |letter| print "#{letter} " }
+  puts
 end
 
 puts 'Welcome to Hangman!'
@@ -32,7 +40,7 @@ loop do
 
   case selection
   when 'New Game'
-    game = Game.new(prompt.ask('Enter Player Name'), word_list[rand(word_list.length + 1)])
+    game = Game.new(prompt.ask('Enter Game Name'), word_list[rand(word_list.length + 1)])
   when 'Saved Game'
     puts 'You chose saved game'
   when 'Quit'
